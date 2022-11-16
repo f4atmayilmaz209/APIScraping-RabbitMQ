301a78b6481f0cf46e45112dea55db1379e7cd71
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2022-11-16 10:45:23 +0300

    Scraping -RabbitMQ
    Created app that scrape with API.

---
+++ sender.py
@@ -0,0 +1,27 @@
+import pika, os, logging, time,json
+logging.basicConfig()
+
+url = os.environ.get('CLOUDAMQP_URL','amqp://guest:guest@localhost/%2f')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello', durable=True)
+
+
+with open("flower.json","r") as dd:
+    for mes in dd.readlines():
+        print(mes)
+        channel.basic_publish(exchange='',
+                            routing_key='hello',
+                            body=json.dumps(mes),
+                            properties=pika.BasicProperties(
+                                delivery_mode = 2, # make message persistent
+                            ))
+
+        print(time.sleep(10))
+
+
+# latest RabbitMQ 3.11
+#docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management
---
+++ test.py
@@ -0,0 +1,68 @@
+import requests 
+import pandas as pd
+import csv
+import json
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
+
+CLIENT_ID=os.getenv("CLIENT_ID")
+SECRET_KEY=os.getenv("SECRET_KEY")
+
+auth=requests.auth.HTTPBasicAuth(CLIENT_ID,SECRET_KEY)
+data={
+    'grant_type':'password',
+    'username':'fatmayilmaz209',
+    'password':os.getenv("PASSWORD")
+}
+
+headers={'User-Agent':'MyAPI/0.0.1'}
+res=requests.post('https://www.reddit.com/api/v1/access_token',auth=auth,data=data,headers=headers)
+TOKEN=res.json()['access_token']
+headers={**headers, **{'Authorization':f'bearer {TOKEN}'}}
+
+limit=input('***Limit nedir?***')
+word=input('***Bir kelime giriniz?***')
+
+
+def running(res):
+    df=pd.DataFrame()
+    for post in res.json()['data']['children']:
+        js={}
+        js['subreddit']=post['data']['subreddit']
+        js['name']=post['data']['name']
+        js['title']=post['data']['title']
+        js['selftext']=post['data']['selftext']
+        js['upvote_ratio']=post['data']['upvote_ratio']
+        js['ups']=post['data']['ups']
+        lastname=post['data']['name']
+        with open(f'{word}.json','a') as ft:
+            json.dump(js, ft)
+            ft.write('\n')
+
+    with open('pagination.csv','a') as fx:
+        fx.writelines(f'{str(lastname)}\n')
+       
+    print(df)
+
+
+
+
+with open('pagination.csv','a') as ftr:
+    try:
+        last_line = csv.reader(ftr)
+        for satir in last_line:
+            a=satir
+        pagi=str(a).replace("['","").replace("']","")
+        res=requests.get(f'https://oauth.reddit.com/r/{word}/new.json?limit={limit}&after={pagi}',headers=headers)
+        running(res)
+        print("TRY")
+    except:
+        res=requests.get(f'https://oauth.reddit.com/r/{word}/new.json?limit={limit}',headers=headers)
+        last_line=res.json()['data']['children'][-1]['data']['name']
+        running(res)
+        print("except")
+
+
+
